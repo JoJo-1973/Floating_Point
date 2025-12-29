89440c769d1f2479b4fa869a7bb51520d9f42bbc
JoJo-1973 <stefano.priore@gmail.com>
2025-12-30 00:09:07 +0100

    Completed documentation of fp_trig.asm	and updated macro names

--- fp_trig.asm
+++ fp_trig.asm
@@ -2,11 +2,22 @@
 ; Trigonometric Functions
 ; -----------------------
 
-; Macro SIN_FAC: FAC = SIN(FAC)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
+; Title:                  MACRO: Compute the sine and store the result in FAC
+; Name:                   SIN_FAC
+;                         SIN_Mem
+;                         SIN_Ptr
+; Description:            Compute the sine of a Microsoft Binary Format floating point number and store the result in FAC.
+;                         The data can be located in FAC, at an absolute memory address or referenced to by a pointer.
+; Input parameters:       addr_:      a memory address
+;                         ptr_:       a pointer
+;                         preserve_ : ARG is destroyed by the operation unless this parameter is <> 0
+; Output parameters:      ---
+; Altered registers:      .A, .X, .Y
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro SIN_FAC preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   jsr SIN
@@ -14,48 +25,50 @@
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro SIN_MEM: FAC = SIN(Memory)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro SIN_MEM addr_, preserve_ {
+!macro SIN_Mem addr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Mem addr_
-
-  +SIN_FAC 0
+  jsr SIN
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro SIN_PTR: FAC = SIN((Pointer))
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro SIN_PTR ptr_, preserve_ {
+!macro SIN_Ptr ptr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Ptr ptr_
-
-  +SIN_FAC 0
+  jsr SIN
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro COS_FAC: FAC = COS(FAC)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
+; Title:                  MACRO: Compute the cosine and store the result in FAC
+; Name:                   COS_FAC
+;                         COS_Mem
+;                         COS_Ptr
+; Description:            Compute the cosine of a Microsoft Binary Format floating point number and store the result in FAC.
+;                         The data can be located in FAC, at an absolute memory address or referenced to by a pointer.
+; Input parameters:       addr_:      a memory address
+;                         ptr_:       a pointer
+;                         preserve_ : ARG is destroyed by the operation unless this parameter is <> 0
+; Output parameters:      ---
+; Altered registers:      .A, .X, .Y
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro COS_FAC preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   jsr COS
@@ -63,48 +76,50 @@
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro COS_MEM: FAC = COS(Memory)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro COS_MEM addr_, preserve_ {
+!macro COS_Mem addr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Mem addr_
-
-  +COS_FAC 0
+  jsr COS
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro COS_PTR: FAC = COS((Pointer))
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro COS_PTR ptr_, preserve_ {
+!macro COS_Ptr ptr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Ptr ptr_
-
-  +COS_FAC 0
+  jsr COS
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro TAN_FAC: FAC = TAN(FAC)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
+; Title:                  MACRO: Compute the tangent and store the result in FAC
+; Name:                   TAN_FAC
+;                         TAN_Mem
+;                         TAN_Ptr
+; Description:            Compute the tangent of a Microsoft Binary Format floating point number and store the result in FAC.
+;                         The data can be located in FAC, at an absolute memory address or referenced to by a pointer.
+; Input parameters:       addr_:      a memory address
+;                         ptr_:       a pointer
+;                         preserve_ : ARG is destroyed by the operation unless this parameter is <> 0
+; Output parameters:      ---
+; Altered registers:      .A, .X, .Y
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro TAN_FAC preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   jsr TAN
@@ -112,48 +127,50 @@
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro TAN_MEM: FAC = TAN(Memory)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro TAN_MEM addr_, preserve_ {
+!macro TAN_Mem addr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Mem addr_
-
-  +TAN_FAC 0
+  jsr TAN
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro TAN_PTR: FAC = TAN((Pointer))
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro TAN_PTR ptr_, preserve_ {
+!macro TAN_Ptr ptr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Ptr ptr_
-
-  +TAN_FAC 0
+  jsr TAN
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro ATN_FAC: FAC = ATN(FAC)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
+; Title:                  MACRO: Compute the arctangent and store the result in FAC
+; Name:                   ATN_FAC
+;                         ATN_Mem
+;                         ATN_Ptr
+; Description:            Compute the arctangent of a Microsoft Binary Format floating point number and store the result in FAC.
+;                         The data can be located in FAC, at an absolute memory address or referenced to by a pointer.
+; Input parameters:       addr_:      a memory address
+;                         ptr_:       a pointer
+;                         preserve_ : ARG is destroyed by the operation unless this parameter is <> 0
+; Output parameters:      ---
+; Altered registers:      .A, .X, .Y
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro ATN_FAC preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   jsr ATN
@@ -161,39 +178,30 @@
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro ATN_MEM: FAC = ATN(Memory)
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro ATN_MEM addr_, preserve_ {
+!macro ATN_Mem addr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Mem addr_
-
-  +ATN_FAC 0
+  jsr ATN
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }
 
-; Macro ATN_PTR: FAC = ATN((Pointer))
-; ARG is destroyed in the process unless 'preserve_' is <> 0.
-!macro ATN_PTR ptr_, preserve_ {
+!macro ATN_Ptr ptr_, preserve_ {
   !if (preserve_) {
-    +Store_ARG_to_Mem STACK            ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
+    +Store_ARG_to_Mem STACK     ; Power routine messes with contents of _SCRATCH_2, so we need a different place to save ARG.
   }
 
   +Load_FAC_from_Ptr ptr_
-
-  +ATN_FAC 0
+  jsr ATN
 
   !if (preserve_) {
     +Load_ARG_from_Mem STACK
   }
-  +Adjust_Signs
 }

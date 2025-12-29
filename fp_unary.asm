157156b5e80590bfc294c0481b8e2a86211f8af9
JoJo-1973 <stefano.priore@gmail.com>
2025-12-29 17:39:41 +0100

    Updated documentation of fp_unary.asm

--- fp_unary.asm
+++ fp_unary.asm
@@ -2,40 +2,56 @@
 ; Unary Functions
 ; ---------------
 
-; Macro Negate_[FAC/ARG]: Change the sign of FAC/ARG.
+; Title:                  MACRO: Negate FAC or ARG
+; Name:                   Negate_FAC
+;                         Negate_ARG
+; Description:            Flip the sign of the value stored in FAC or ARG.
+; Input parameters:       ---
+; Output parameters:      ---
+; Altered registers:      .A
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro Negate_FAC {
   lda FACSGN
   eor #$FF
   sta FACSGN
-
-  +Adjust_Signs
 }
 
 !macro Negate_ARG {
   lda ARGSGN
   eor #$FF
   sta ARGSGN
-
-  +Adjust_Signs
 }
 
-; Macro Abs_[FAC/ARG]: Compute the absolute value of FAC/ARG.
+; Title:                  MACRO: Absolute value of FAC or ARG
+; Name:                   Abs_FAC
+;                         Abs_ARG
+; Description:            Compute the absolute value of FAC or ARG by clearing MSB of sign.
+; Input parameters:       ---
+; Output parameters:      ---
+; Altered registers:      ---
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro Abs_FAC {
   lsr FACSGN                    ; Just replace bit #7 of sign with 0.
-
-  +Adjust_Signs
 }
 
 !macro Abs_ARG {
   lsr ARGSGN                    ; Just replace bit #7 of sign with 0.
-
-  +Adjust_Signs
 }
 
-; Macro Sign_[FAC/ARG]: Sign of FAC/ARG.
-; .A = $FF if FAC/ARG < 0
-; .A = $00 if FAC/ARG = 0
-; .A = $01 if FAC/ARG > 0
+; Title:                  MACRO: Return sign of FAC or ARG
+; Name:                   Sign_FAC
+;                         Sign_ARG
+; Description:            Return the sign of FAC or ARG in the accumulator.
+;                           .A = $FF if FAC/ARG < 0
+;                           .A = $00 if FAC/ARG = 0
+;                           .A = $01 if FAC/ARG > 0
+; Input parameters:       ---
+; Output parameters:      ---
+; Altered registers:      .A
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro Sign_FAC {
   jsr SIGN
 }
@@ -52,9 +68,15 @@
 @Exit
 }
 
-; Macro Int_[FAC/ARG]: Rounds FAC/ARG towards minus infinity.
-; Operation on ARG is discouraged because of the swaps of FAC and ARG:
-; the macro is provided just in case.
+; Title:                  MACRO: Round FAC or ARG towards negative infinity
+; Name:                   Int_FAC
+;                         Int_ARG
+; Description:            Return FAC or ARG rounded towards negative infinity, i.e. 1.2 becomes 1 and -1.2 becomes -2.
+; Input parameters:       ---
+; Output parameters:      ---
+; Altered registers:      .A, .X, .Y
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro Int_FAC {
   jsr INT
 }
@@ -65,7 +87,17 @@
   +Swap_FAC_and_ARG
 }
 
-; Macro Multiply_[FAC/ARG]_by_2: Double FAC/ARG by exponent manipulation.
+; Title:                  MACRO: Multiply or divide FAC or ARG by 2
+; Name:                   Multiply_FAC_by_2
+;                         Multiply_ARG_by_2
+;                         Divide_FAC_by_2
+;                         Divide_ARG_by_2
+; Description:            Multiply or divide FAC or ARG by 2 manipulating the exponent byte rather than using division.
+; Input parameters:       ---
+; Output parameters:      ---
+; Altered registers:      ---
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
 !macro Multiply_FAC_by_2 {
   inc FACEXP
 }
@@ -74,7 +106,6 @@
   inc ARGEXP
 }
 
-; Macro Divide_[FAC/ARG]_by_2: Halve FAC/ARG by exponent manipulation.
 !macro Divide_FAC_by_2 {
   dec FACEXP
 }

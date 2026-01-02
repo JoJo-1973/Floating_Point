632887a2b439d8ed17ddba3772570d792460777b
JoJo-1973 <stefano.priore@gmail.com>
2026-01-02 23:08:28 +0100

    Added support for SGN

--- fp_unary.asm
+++ fp_unary.asm
@@ -117,6 +117,44 @@
   jsr SIGN
 }
 
+; Title:                  MACRO: Return the sign of a floating point number and store the result in FAC
+; Name:                   SGN_FAC
+;                         SGN_ARG
+;                         SGN_Mem
+;                         SGN_Ptr
+; Description:            Return the sign of a Microsoft Binary Format floating point number and store the result in FAC.
+;                         SGN_ARG is an exception because it operates in place rather than storing the result in FAC.
+;                         The data can be located in FAC, in ARG, at an absolute memory address or referenced to by a pointer.
+;                           FAC = -1 if data < 0.
+;                           FAC = 0 if data = 0.
+;                           FAC = 1 if data > 0.
+; Input parameters:       addr_: a memory address
+;                         ptr_:  a pointer
+; Output parameters:      .A
+; Altered registers:      .A, .Y
+; Altered zero-page:      ---
+; External dependencies:  standard.asm, symbols.asm, kernal.asm
+!macro SGN_FAC {
+  jsr SGN
+}
+
+!macro SGN_ARG {
+  +Swap_FAC_and_ARG
+  jsr SGN
+  +Swap_FAC_and_ARG
+
+}
+
+!macro SGN_Mem addr_ {
+  +Load_FAC_from_Mem addr_
+  jsr SGN
+}
+
+!macro SGN_Ptr ptr_ {
+  +Load_FAC_from_Ptr ptr_
+  jsr SGN
+}
+
 ; Title:                  MACRO: Round a floating point number towards negative infinity and store the result in FAC
 ; Name:                   INT_FAC
 ;                         INT_ARG
